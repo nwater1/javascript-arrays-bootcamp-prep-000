3539463741b24f0cef09e431deddc741b37816b9
Jatupon Jantri <jameball007@gmail.com>
2018-01-24 06:28:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,8 +7,8 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   
-  var array = addElementToBeginningOfArray(array, element)
-  return array
+  var array1 = addElementToBeginningOfArray(array, element)
+  return array1
   
   
   
