e5b729043ca44c83be1206670a7e2f0126e6abf2
Jatupon Jantri <jameball007@gmail.com>
2018-01-24 05:49:15 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,34 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  var array = [element, ...array]
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
+  return more_array = addElementToBeginningOfArray(array, element)
+  
+  
+  
+}
+
+function addElementToEndOfArray(array, element) {
+  var array = [...array, element]
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  var more_array = [...array, element]
+  return more_array
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
