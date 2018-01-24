36c546861b980390cf9b00f95a6ceb5b3b245d5f
Jatupon Jantri <jameball007@gmail.com>
2018-01-24 05:57:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,7 +34,9 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1)
+  array = array.slice(1)
   return array
 }
+
+
 
